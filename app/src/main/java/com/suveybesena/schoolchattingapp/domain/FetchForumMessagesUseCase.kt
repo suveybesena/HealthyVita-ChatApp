848054a27ed4c194f409af0e6d4256544715a018
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-04-23 14:05:06 +0300

    add forumUseCases

---
+++ app/src/main/java/com/suveybesena/schoolchattingapp/domain/FetchForumMessagesUseCase.kt
@@ -0,0 +1,34 @@
+package com.suveybesena.schoolchattingapp.domain
+
+import com.suveybesena.schoolchattingapp.common.Resource
+import com.suveybesena.schoolchattingapp.data.repository.Repository
+import com.suveybesena.schoolchattingapp.presentation.forum.ForumModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import java.lang.Exception
+import javax.inject.Inject
+
+class FetchForumMessagesUseCase @Inject constructor(val repository: Repository) {
+
+    suspend fun invoke() = flow {
+        emit(Resource.Loading)
+        try {
+            val forumList = ArrayList<ForumModel>()
+            repository.fetchForumMessagesFromFirebase().forEach { document ->
+                val forumMessages = document.get("forumMessage") as String
+                val userId = document.get("userId") as String
+                val time = document.get("time") as Long
+                val messageId = document.get("messageId") as String
+                val userImage = document.get("userImage") as String
+                val userName = document.get("userName") as String
+                val listForum =
+                    ForumModel(forumMessages, userId, time, messageId, userImage, userName)
+                forumList.add(listForum)
+                emit(Resource.Success(forumList))
+            }
+        } catch (e: Exception) {
+            emit(Resource.Error(e.localizedMessage))
+        }
+    }.flowOn(Dispatchers.IO)
+}
