eec6b78222a2d8f1be01999b2822833435171250
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-04-25 01:16:25 +0300

    add addMessageImageToStorage

--- app/src/main/java/com/suveybesena/schoolchattingapp/data/firebase/storage/source/FirebaseStorageSource.kt
+++ app/src/main/java/com/suveybesena/schoolchattingapp/data/firebase/storage/source/FirebaseStorageSource.kt
@@ -5,4 +5,5 @@ import android.net.Uri
 interface FirebaseStorageSource {
     suspend fun addImageToStorageForDoctors(profileImage: Uri, currentUserId: String): String
     suspend fun addImageToStorageForPatients(profileImage: Uri, currentUserId: String): String
+    suspend fun addMessageImageToStorage(profileImage: Uri, currentUserId: String): String
 }
