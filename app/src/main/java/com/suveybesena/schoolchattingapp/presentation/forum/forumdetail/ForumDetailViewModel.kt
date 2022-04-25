bf084b9fda030905ba53696b1e075a9cb2d9b94e
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-04-26 01:07:59 +0300

    add forumAnswerViewModel

--- app/src/main/java/com/suveybesena/schoolchattingapp/presentation/forum/forumdetail/ForumDetailViewModel.kt
+++ app/src/main/java/com/suveybesena/schoolchattingapp/presentation/forum/forumdetail/ForumDetailViewModel.kt
@@ -1,9 +1,57 @@
 package com.suveybesena.schoolchattingapp.presentation.forum.forumdetail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.suveybesena.schoolchattingapp.common.Resource
+import com.suveybesena.schoolchattingapp.domain.AddForumAnswerUseCase
+import com.suveybesena.schoolchattingapp.domain.AddForumMessageUseCase
+import com.suveybesena.schoolchattingapp.domain.FetchForumAnswersUseCase
+import com.suveybesena.schoolchattingapp.presentation.forum.forumfeed.ForumState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ForumDetailViewModel @Inject constructor() : ViewModel() {
+class ForumDetailViewModel @Inject constructor(
+    private val addForumAnswerUseCase: AddForumAnswerUseCase,
+    private val fetchForumAnswersUseCase: FetchForumAnswersUseCase) : ViewModel() {
+
+    private val uiState = MutableStateFlow(ForumDetailState())
+    val _uiState: StateFlow<ForumDetailState> = uiState.asStateFlow()
+
+    fun handleEvent ( event : ForumDetailEvent){
+        when(event){
+            is ForumDetailEvent.AddForumMessageUseCase->{
+                addForumAnswerToFirebase(event.answers)
+            }
+            is ForumDetailEvent.FetchForumMessageUseCase->{
+                fetchForumAnswers(event.messageId)
+            }
+        }
+    }
+
+    private fun fetchForumAnswers(messageId : String) {
+        viewModelScope.launch {
+            fetchForumAnswersUseCase.invoke(messageId).collect{resultState->
+                when(resultState){
+                    is Resource.Success->{
+                        uiState.update { state->
+                            state.copy( answerList = resultState.data as List<ForumDetailModel>)
+                        }
+                    }
+                }
+            }
+
+            }
+        }
+
+    private fun addForumAnswerToFirebase(answersModel: ForumDetailModel) {
+        viewModelScope.launch {
+            addForumAnswerUseCase.invoke(answersModel).collect{}
+        }
+    }
 }
