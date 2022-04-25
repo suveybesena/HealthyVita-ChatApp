72bfd99bf769e6b46196efda37f213e0a916fe31
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-04-26 01:05:42 +0300

    add forumAnswer adapter

---
+++ app/src/main/java/com/suveybesena/schoolchattingapp/presentation/forum/forumdetail/ForumAnswersAdapter.kt
@@ -0,0 +1,46 @@
+package com.suveybesena.schoolchattingapp.presentation.forum.forumdetail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.suveybesena.schoolchattingapp.common.downloadImage
+import com.suveybesena.schoolchattingapp.databinding.ItemForumBinding
+
+class ForumAnswersAdapter : RecyclerView.Adapter<ForumAnswersAdapter.AnswersVH>() {
+    class AnswersVH(val binding : ItemForumBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+    private val differCallBack = object : DiffUtil.ItemCallback<ForumDetailModel>() {
+        override fun areItemsTheSame(
+            oldItem: ForumDetailModel,
+            newItem: ForumDetailModel
+        ): Boolean {
+            return true
+        }
+
+        override fun areContentsTheSame(
+            oldItem: ForumDetailModel,
+            newItem: ForumDetailModel
+        ): Boolean {
+            return true
+        }
+    }
+    val differ = AsyncListDiffer(this, differCallBack)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForumAnswersAdapter.AnswersVH {
+        val binding = ItemForumBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return AnswersVH(binding)
+    }
+
+    override fun onBindViewHolder(holder: AnswersVH, position: Int) {
+        val list = differ.currentList[position]
+        holder.binding.apply {
+            tvForum.text =list.message
+        }
+    }
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+}
