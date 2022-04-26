438812ada3ac753e29264e386770a4dd4c585d93
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-04-27 00:33:48 +0300

    add patientMessages

--- app/src/main/java/com/suveybesena/schoolchattingapp/presentation/doctors/DoctorsFragment.kt
+++ app/src/main/java/com/suveybesena/schoolchattingapp/presentation/doctors/DoctorsFragment.kt
@@ -8,8 +8,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.suveybesena.schoolchattingapp.R
+import com.suveybesena.schoolchattingapp.common.downloadImage
 import com.suveybesena.schoolchattingapp.databinding.FragmentDoctorsBinding
+import com.suveybesena.schoolchattingapp.presentation.profile.ProfileEvent
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -20,6 +23,7 @@ class DoctorsFragment : Fragment() {
     private lateinit var binding: FragmentDoctorsBinding
     private val viewModel: DoctorsViewModel by viewModels()
     private lateinit var doctorsAdapter: DoctorsAdapter
+    private lateinit var patientsAdapter: PatientMessagesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,28 +32,40 @@ class DoctorsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentDoctorsBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupRecyclerView()
-        initListeners()
+        setupDoctorRecyclerView()
         observeData()
+        initListeners()
     }
 
-    private fun setupRecyclerView() {
+    private fun setupDoctorRecyclerView() {
         doctorsAdapter = DoctorsAdapter(object : OnItemQuestionClick {
             override fun onDoctorItemClick(doctorInfo: DoctorFeedModel) {
                 goChatFragment(doctorInfo)
             }
         })
         binding.apply {
-            rvDoctors.adapter = doctorsAdapter
+            rvConversations.adapter = doctorsAdapter
         }
     }
+
+    private fun setupPatientRecyclerView() {
+        patientsAdapter = PatientMessagesAdapter(object : OnItemQuestionClick {
+            override fun onDoctorItemClick(doctorInfo: DoctorFeedModel) {
+                goChatFragment(doctorInfo)
+            }
+        })
+        binding.apply {
+            rvConversations.adapter = patientsAdapter
+        }
+    }
+
 
     private fun goChatFragment(doctorInfo: DoctorFeedModel) {
         val bundle = Bundle()
@@ -58,16 +74,42 @@ class DoctorsFragment : Fragment() {
     }
 
     private fun observeData() {
-        viewModel.handleEvent(DoctorsFeedEvent.FetchDoctorsData)
+        val currentUserId = FirebaseAuth.getInstance().currentUser?.uid
+        viewModel.handleEvent(DoctorsFeedEvent.FetchDoctorData(currentUserId))
+
         lifecycleScope.launch {
             viewModel._uiState.collect { state ->
-                state.list.let { list ->
-                    doctorsAdapter.differ.submitList(list)
+                state.doctorInfo.let { doctor ->
+                    if (doctor?.userId != null) {
+                        binding.tvConversation.text = "Bekleyen Görüşmeleriniz"
+                        setupPatientRecyclerView()
+                        viewModel.handleEvent(DoctorsFeedEvent.FetchPatientMessages(currentUserId))
+                        lifecycleScope.launch {
+                            viewModel._uiState.collect{state->
+                                state.patientMessage.let { list->
+                                    patientsAdapter.differ.submitList(list)
+                                   println(list)
+                                }
+                            }
+                        }
+
+                    } else {
+                        binding.tvConversation.text = "Görüşme başlatmak için doktor seçiniz."
+                        setupDoctorRecyclerView()
+                        viewModel.handleEvent(DoctorsFeedEvent.FetchDoctorsData)
+                        lifecycleScope.launch {
+                            viewModel._uiState.collect { state ->
+                                state.list.let { list ->
+                                    doctorsAdapter.differ.submitList(list)
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
 
+    }
     private fun initListeners() {
     }
 }
--- app/src/main/java/com/suveybesena/schoolchattingapp/presentation/doctors/DoctorsViewModel.kt
+++ app/src/main/java/com/suveybesena/schoolchattingapp/presentation/doctors/DoctorsViewModel.kt
@@ -3,14 +3,23 @@ package com.suveybesena.schoolchattingapp.presentation.doctors
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.suveybesena.schoolchattingapp.common.Resource
+import com.suveybesena.schoolchattingapp.domain.FetchDoctorInfoUseCase
 import com.suveybesena.schoolchattingapp.domain.FetchDoctorsUseCase
+import com.suveybesena.schoolchattingapp.domain.FetchMessageFromPatientUseCase
+import com.suveybesena.schoolchattingapp.domain.FetchPatientInfoUseCase
+import com.suveybesena.schoolchattingapp.presentation.profile.ProfileEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DoctorsViewModel @Inject constructor(private val fetchDoctorsUseCase: FetchDoctorsUseCase) :
+class DoctorsViewModel @Inject constructor(
+    private val fetchDoctorsUseCase: FetchDoctorsUseCase,
+    private val fetchDoctorInfoUseCase: FetchDoctorInfoUseCase,
+    private val fetchPatientInfoUseCase: FetchPatientInfoUseCase,
+    private val fetchPatientMessageUseCase : FetchMessageFromPatientUseCase
+) :
     ViewModel() {
 
     private val uiState = MutableStateFlow(DoctorsFeedState())
@@ -19,12 +28,66 @@ class DoctorsViewModel @Inject constructor(private val fetchDoctorsUseCase: Fetc
     fun handleEvent(event: DoctorsFeedEvent) {
         when (event) {
             is DoctorsFeedEvent.FetchDoctorsData -> {
-                fetchTeacherData()
+                fetchDoctorsData()
+            }
+            is DoctorsFeedEvent.FetchDoctorData -> {
+                event.currentUserId?.let { fetchDoctorData(it) }
+            }
+            is DoctorsFeedEvent.FetchPatientData -> {
+                event.currentUserId?.let { fetchPatientData(it) }
+            }
+            is DoctorsFeedEvent.FetchPatientMessages->{
+                event.currentUserId?.let { fetchPatientMessage(it) }
             }
         }
     }
 
-    private fun fetchTeacherData() {
+    private fun fetchPatientMessage(currentUserId: String) {
+        viewModelScope.launch {
+            //println("wwwqqqqqwwwww")
+            fetchPatientMessageUseCase.invoke(currentUserId).collect{ resultState->
+               // println("ssss")
+                when(resultState){
+                    is Resource.Success->{
+                       // println("wwwwwwww")
+                        uiState.update { state->
+                            state.copy(patientMessage = resultState.data)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun fetchPatientData(currentUserId: String) {
+        viewModelScope.launch {
+            fetchPatientInfoUseCase.invoke(currentUserId).collect { resultState ->
+                when (resultState) {
+                    is Resource.Success -> {
+                        uiState.update { state ->
+                            state.copy(patientInfo = resultState.data)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun fetchDoctorData(currentUserId: String) {
+        viewModelScope.launch {
+            fetchDoctorInfoUseCase.invoke(currentUserId).collect { resultState ->
+                when (resultState) {
+                    is Resource.Success -> {
+                        uiState.update { state ->
+                            state.copy(doctorInfo = resultState.data)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun fetchDoctorsData() {
         viewModelScope.launch {
             fetchDoctorsUseCase.invoke().collect { resultState ->
                 when (resultState) {
