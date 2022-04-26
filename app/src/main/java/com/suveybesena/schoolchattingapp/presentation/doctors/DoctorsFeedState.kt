7d8cfdadc026b117177eb81b9ec322a91c0073c1
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-04-27 00:33:17 +0300

    add new events

--- app/src/main/java/com/suveybesena/schoolchattingapp/presentation/doctors/DoctorsFeedState.kt
+++ app/src/main/java/com/suveybesena/schoolchattingapp/presentation/doctors/DoctorsFeedState.kt
@@ -1,7 +1,12 @@
 package com.suveybesena.schoolchattingapp.presentation.doctors
 
+import com.suveybesena.schoolchattingapp.presentation.forum.UserModel
+
 data class DoctorsFeedState(
     val isLoading: Boolean = false,
     val list: List<DoctorFeedModel>? = null,
-    val error: Boolean? = false
+    val error: Boolean? = false,
+    val doctorInfo: UserModel? = null,
+    val patientInfo: UserModel? = null,
+    val patientMessage : List<String>? = null
 )
