8b94e2bc3ce681184ac3b228684c08d087bd1fb9
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-04-27 00:33:10 +0300

    add new events

--- app/src/main/java/com/suveybesena/schoolchattingapp/presentation/doctors/DoctorsFeedEvent.kt
+++ app/src/main/java/com/suveybesena/schoolchattingapp/presentation/doctors/DoctorsFeedEvent.kt
@@ -1,5 +1,11 @@
 package com.suveybesena.schoolchattingapp.presentation.doctors
 
+import com.suveybesena.schoolchattingapp.presentation.profile.ProfileEvent
+
 sealed class DoctorsFeedEvent {
     object FetchDoctorsData : DoctorsFeedEvent()
+    data class FetchDoctorData(val currentUserId: String?) : DoctorsFeedEvent()
+    data class FetchPatientData(val currentUserId: String?) : DoctorsFeedEvent()
+    data class FetchPatientMessages(val currentUserId: String?) : DoctorsFeedEvent()
+
 }
