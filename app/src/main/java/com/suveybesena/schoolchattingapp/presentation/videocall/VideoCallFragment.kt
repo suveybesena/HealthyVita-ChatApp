4a7d0ef0a6a20c70acb6bbcc678fd4a8a87cbdd7
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-04-30 01:05:11 +0300

    remove setZOrderMediaOverlay from remoteVideo

--- app/src/main/java/com/suveybesena/schoolchattingapp/presentation/videocall/VideoCallFragment.kt
+++ app/src/main/java/com/suveybesena/schoolchattingapp/presentation/videocall/VideoCallFragment.kt
@@ -27,11 +27,13 @@ class VideoCallFragment : Fragment() {
     private val PERMISSION_REQ_ID_RECORD_AUDIO = 22
     private val PERMISSION_REQ_ID_CAMERA = PERMISSION_REQ_ID_RECORD_AUDIO + 1
     private val APP_ID = "f7e1c5ea54594c9388381270e5458d3c"
-    private val CHANNEL = "test"
+    private var CHANNEL: String? = null
     private val TOKEN =
-        "006f7e1c5ea54594c9388381270e5458d3cIADVERJ8I7vfa0iEuBK24H5Tx/ZFVhBbabNwmBNk2DPmDAx+f9gAAAAAEADchOPNqgtoYgEAAQCpC2hi"
+        "006f7e1c5ea54594c9388381270e5458d3cIABl6FtIHTe3wzlXeK516LIjhcynlDD3R1fjy8a/96hOeQx+f9gAAAAAEAC3KIpZ2qRtYgEAAQDbpG1i"
     private var mRtcEngine: RtcEngine? = null
     private lateinit var binding: FragmentVideoCallBinding
+    private var userRole = 0
+    private val navArgs: VideoCallFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,87 +46,111 @@ class VideoCallFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-       if (checkSelfPermission(Manifest.permission.RECORD_AUDIO, PERMISSION_REQ_ID_RECORD_AUDIO) && checkSelfPermission(Manifest.permission.CAMERA, PERMISSION_REQ_ID_CAMERA)) {
-           initializeAndJoinChannel()
-       }
-       initListeners()
+
+        val bundle = navArgs.roomInfo
+        userRole = bundle.userRole
+        CHANNEL = bundle.channelName
+
+
+        if (checkSelfPermission(
+                Manifest.permission.RECORD_AUDIO,
+                PERMISSION_REQ_ID_RECORD_AUDIO
+            ) && checkSelfPermission(Manifest.permission.CAMERA, PERMISSION_REQ_ID_CAMERA)
+        ) {
+            initializeAndJoinChannel()
+        }
+        initListeners()
     }
 
     private fun initListeners() {
         binding.bvCloseCall.setOnClickListener {
             onClose()
         }
-         binding.bvMCameraSwitch.setOnClickListener { view ->
-             onSwitchCamera(view)
-         }
-     }
+        binding.bvMCameraSwitch.setOnClickListener { view ->
+            onSwitchCamera(view)
+        }
+    }
 
-     private fun onSwitchCamera(view: View?) {
-         mRtcEngine!!.switchCamera()
-     }
-     private fun onClose() {
-         findNavController().navigate(R.id.action_videoCallFragment_to_videoRoomFragment)
-     }
+    private fun onSwitchCamera(view: View?) {
+        mRtcEngine!!.switchCamera()
+    }
+
+    private fun onClose() {
+        findNavController().navigate(R.id.action_videoCallFragment_to_videoRoomFragment)
+    }
 
 
-     private fun checkSelfPermission(permission: String, requestCode: Int): Boolean {
-         if (ContextCompat.checkSelfPermission(requireContext(), permission) !=
-             PackageManager.PERMISSION_GRANTED
-         ) {
-             ActivityCompat.requestPermissions(
-                 requireActivity(),
-                 arrayOf(permission),
-                 requestCode
-             )
-             return false
-         }
-         return true
-     }
+    private fun checkSelfPermission(permission: String, requestCode: Int): Boolean {
+        if (ContextCompat.checkSelfPermission(requireContext(), permission) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(permission),
+                requestCode
+            )
+            return false
+        }
+        return true
+    }
 
-     private val mRtcEventHandler = object : IRtcEngineEventHandler() {
-         override fun onUserJoined(uid: Int, elapsed: Int) {
-             requireActivity().runOnUiThread {
-                 setupRemoteVideo(uid)
-             }
-         }
+    private val mRtcEventHandler = object : IRtcEngineEventHandler() {
+        override fun onUserJoined(uid: Int, elapsed: Int) {
+            requireActivity().runOnUiThread {
+                setupRemoteVideo(uid)
+            }
+        }
 
-         override fun onLeaveChannel(stats: RtcStats?) {
-             super.onLeaveChannel(stats)
-             onDestroy()
-         }
-     }
+        override fun onLeaveChannel(stats: RtcStats?) {
+            super.onLeaveChannel(stats)
+            onDestroy()
+        }
+    }
 
-     private fun initializeAndJoinChannel() {
-         try {
-             mRtcEngine = RtcEngine.create(context, APP_ID, mRtcEventHandler)
-         } catch (e: Exception) {
-         }
+    private fun initializeAndJoinChannel() {
+        try {
+            mRtcEngine = RtcEngine.create(context, APP_ID, mRtcEventHandler)
+        } catch (e: Exception) {
+        }
+        mRtcEngine!!.setChannelProfile(Constants.CHANNEL_PROFILE_LIVE_BROADCASTING)
+        mRtcEngine!!.setClientRole(userRole)
 
-         mRtcEngine!!.enableVideo()
+        mRtcEngine!!.enableVideo()
 
-         val localContainer = binding.localVideoViewContainer
-         val localFrame = RtcEngine.CreateRendererView(context)
-         localFrame.setZOrderMediaOverlay(true)
-         localContainer.addView(localFrame)
+        if (userRole == 1) {
+            setupLocaleVideo()
+            val localContainer = binding.localVideoViewContainer
+            localContainer.isVisible = true
+        } else {
+            val localContainer = binding.localVideoViewContainer
+            localContainer.isVisible = false
+        }
+        mRtcEngine!!.joinChannel(TOKEN, CHANNEL, "", 0)
+    }
 
-         mRtcEngine!!.setupLocalVideo(VideoCanvas(localFrame, VideoCanvas.RENDER_MODE_FIT, 0))
-         mRtcEngine!!.joinChannel(TOKEN, CHANNEL, "", 0)
-     }
+    private fun setupLocaleVideo() {
+        val localContainer = binding.localVideoViewContainer
+        val localFrame = RtcEngine.CreateRendererView(context)
+        localFrame.setZOrderMediaOverlay(true)
+        localContainer.addView(localFrame)
+        mRtcEngine!!.setupLocalVideo(VideoCanvas(localFrame, VideoCanvas.RENDER_MODE_FIT, 0))
 
-     private fun setupRemoteVideo(uid: Int) {
-         val remoteContainer = binding.remoteVideoViewContainer as FrameLayout
-         if (remoteContainer.childCount >=1){
-             return
-         }
-         val remoteFrame = RtcEngine.CreateRendererView(context)
-         remoteFrame.setZOrderMediaOverlay(true)
-         remoteContainer.addView(remoteFrame)
-         mRtcEngine!!.setupRemoteVideo(VideoCanvas(remoteFrame, VideoCanvas.RENDER_MODE_FIT, uid))
-     }
+    }
 
-     override fun onDestroy() {
-         super.onDestroy()
-         mRtcEngine?.leaveChannel()
-         RtcEngine.destroy()
-     }
+    private fun setupRemoteVideo(uid: Int) {
+        val remoteContainer = binding.remoteVideoViewContainer as FrameLayout
+       if (remoteContainer.childCount >= 1) {
+           return
+       }
+        val remoteFrame = RtcEngine.CreateRendererView(context)
+        remoteContainer.addView(remoteFrame)
+        mRtcEngine!!.setupRemoteVideo(VideoCanvas(remoteFrame, VideoCanvas.RENDER_MODE_FIT, uid))
+        remoteFrame.tag = uid
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mRtcEngine?.leaveChannel()
+        RtcEngine.destroy()
+    }
 }
