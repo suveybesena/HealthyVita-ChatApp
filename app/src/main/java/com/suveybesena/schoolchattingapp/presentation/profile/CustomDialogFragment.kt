b1ba3bb369edc4074c05669d0e04ab5a6455cda2
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-05-01 01:52:14 +0300

    add resource

---
+++ app/src/main/java/com/suveybesena/schoolchattingapp/presentation/profile/CustomDialogFragment.kt
@@ -0,0 +1,37 @@
+package com.suveybesena.schoolchattingapp.presentation.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.suveybesena.schoolchattingapp.R
+import com.suveybesena.schoolchattingapp.databinding.FragmentCustomDialogBinding
+
+class CustomDialogFragment : DialogFragment() {
+    private lateinit var binding: FragmentCustomDialogBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCustomDialogBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.bvNo.setOnClickListener {
+            dismiss()
+        }
+        binding.bvYes.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            findNavController().navigate(R.id.loginFragment)
+        }
+
+    }
+}
