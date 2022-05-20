2ad803db116c2e81d3a58413d1317a8f88444acc
eleccoder <vidividi12@gmail.com>
2022-05-20 17:34:52 +0300

    inject coroutines

---
+++ app/src/main/java/com/suveybesena/schoolchattingapp/di/CoroutinesQualifiers.kt
@@ -0,0 +1,20 @@
+package com.suveybesena.schoolchattingapp.di
+
+import javax.inject.Qualifier
+
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class MainDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainImmediateDispatcher
