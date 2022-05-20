7f867b08693644fff8d0b5c78bb9a8be77e6b1ea
eleccoder <vidividi12@gmail.com>
2022-05-20 17:36:04 +0300

    change application classpath

---
+++ app/src/main/java/com/suveybesena/schoolchattingapp/OnlineHospitalApplication.kt
@@ -0,0 +1,7 @@
+package com.suveybesena.schoolchattingapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class OnlineHospitalApplication : Application()
